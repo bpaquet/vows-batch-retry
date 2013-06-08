0a5f8558bc4b3f90e3c1fbbde7d2c3e22d8cb7a7
Bertrand Paquet <bpaquet@octo.com>
2013-06-08 17:24:36 +0200

    Add timeout test

---
+++ test/test_failed_async_timeout.js
@@ -0,0 +1,15 @@
+var vows = require('../lib/vows-batch-retry'),
+    assert = require('assert');
+
+vows.describe('vows failed').addBatchRetry({
+  'async assert test': {
+    topic: function() {
+      var callback = this.callback;
+      setTimeout(function() {
+      }, 100);
+    },
+    cb_check: function(t, tt) {
+      assert.equal(t, "toto");
+    }
+  }
+}, 5, 100).export(module);
