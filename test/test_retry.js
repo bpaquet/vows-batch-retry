f8ff3464a0af50c04d5063cb554b90c0945a432a
Bertrand Paquet <bpaquet@octo.com>
2013-06-08 16:10:49 +0200

    Manage timeout

--- test/test_retry.js
+++ test/test_retry.js
@@ -4,8 +4,9 @@ var vows = require('../lib/vows-batch-retry'),
 var std_counter = 0;
 var callback_counter = 0;
 var callback_ep2_counter = 0;
-var callback_exception_counter = 0;
-var callback_topic_counter = 0;
+var callback_async_timeout_counter = 0;
+var callback_async_exception_counter = 0;
+var callback_aysnc_assert_counter = 0;
 
 vows.describe('vows batch retry').addBatchRetry({
 //   'standard test': {
@@ -48,53 +49,70 @@ vows.describe('vows batch retry').addBatchRetry({
     }
   }
 }, 10).addBatchRetry({
-  'callback exception test': {
+  'callback async timeout test': {
+    topic: function() {
+      var callback = this.callback;
+      callback_async_timeout_counter += 1;
+      var wait = 450 - (50 * callback_async_timeout_counter);
+      setTimeout(function() {
+        callback(null, "toto");
+      }, wait);
+    },
+    timeout_check: function(t) {
+      assert.equal(t, "toto");
+    }
+  }
+}, 10, 200).addBatchRetry({
+  'callback async exception test': {
     topic: function() {
       var callback = this.callback;
       setTimeout(function() {
         var result = "myerror";
-        callback_exception_counter += 1;
-        if (callback_exception_counter == 4) {
-          result = null;
+        callback_async_exception_counter += 1;
+        if (callback_async_exception_counter == 4) {
+          result = undefined;
         }
         callback(result, "toto");
       }, 100);
     },
     exception_check: function(t) {
       assert.equal(t, "toto");
-      assert.equal(callback_exception_counter, 4);
+      assert.equal(callback_async_exception_counter, 4);
     }
   }
-}, 10).addBatchRetry({
-//   'callback assert topic test': {
-//     topic: function() {
-//       var callback = this.callback;
-//       setTimeout(function() {
-//         callback_topic_counter += 1;
-//         assert.equal(callback_topic_counter, 8);
-//         callback(null, "toto");
-//       }, 100);
-//     },
-//     exception_check: function(t) {
-//       assert.equal(t, "toto");
-//     }
-//   }
-// }, 10).addBatch({
+}, 10, 500).addBatchRetry({
+  'callback async assert topic test': {
+    topic: function() {
+      var callback = this.callback;
+      setTimeout(function() {
+        callback_aysnc_assert_counter += 1;
+        assert.equal(callback_aysnc_assert_counter, 8);
+        callback(null, "toto");
+      }, 100);
+    },
+    assert_check: function(t) {
+      assert.equal(t, "toto");
+    }
+  }
+}, 10, 200).addBatch({
   'check counters': {
-    check_std_counters: function() {
+    check_std_counter: function() {
       // assert.equal(std_counter, 3);
     },
-    check_callback_counters: function() {
+    check_callback_counter: function() {
       assert.equal(callback_counter, 3);
     },
-    check_callback_ep2_counters: function() {
+    check_callback_ep2_counter: function() {
       assert.equal(callback_ep2_counter, 7);
     },
-    check_callback_exception_counters: function() {
-      assert.equal(callback_exception_counter, 4);
+    check_callback_async_timeout_counter: function() {
+      assert.equal(callback_async_timeout_counter, 6);
     },
-    check_callback_topic_counters: function() {
-      assert.equal(callback_topic_counter, 8);
+    check_callback_async_exception_counter: function() {
+      assert.equal(callback_async_exception_counter, 4);
+    },
+    check_callback_async_assert_counter: function() {
+      assert.equal(callback_aysnc_assert_counter, 8);
     },
   }
 }).export(module);
